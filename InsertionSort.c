69ebdf2b38700240e26cd0cd7e0f4285b960e9ae
Menua <menua.ohanyan@gmail.com>
2020-04-12 15:06:58 +0300

    correction

--- InsertionSort.c
+++ InsertionSort.c
@@ -1,20 +1,26 @@
 
+int* InsertionSort(int* a, int m, int n);
 
 int main()
 {
+	int a[10] ={1,2,4,8,16,32,3,7,20,33};
+	
+	int* b = InsertionSort( a , 6 , 10 );
+}
 
-	int cardsOnTable[6] = {6,5,4,3,2,1};
-	int i = 0;
-	for(int j = 1; j < 6; j++)
+int* InsertionSort(int* cardsOnTable, int m, int n)
+{
+	int x;
+	for(int i = m; i < n; i++)
 	{
-		int key = cardsOnTable[j];
-			i = j - 1;
-		
-		while(cardsOnTable[i] > key)
+		x = cardsOnTable[i];
+		int j = i-1;
+		while(x < cardsOnTable[j] && j >= 0)
 		{
-			cardsOnTable[j] = cardsOnTable[i];
-			
-			cardsOnTable[i] = key;
+			cardsOnTable[j+1] = cardsOnTable[j];
+			j--;
 		}
+			cardsOnTable[j+1] = x;		
 	}
+	return cardsOnTable;
 }
